e4e8d01ab50a80eb6b281e1e40b409c74d88a806
kelifuzhou <cliffchew84@gmail.com>
2022-10-21 00:53:06 +0800

    added glossary

---
+++ pages/Housing Glossary.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+#### Housing Glossary
+
+Thanks my friend, Dean for suggesting I add a glossary to the different housing types. However, given the available resources online, I think it makes more sense for me to just share the links we found. If anyone researches hard enough online, he or she will most probably find the same links as me. I do not earn from any of these links. 
+
+I cannot confirm the availability of these links as well, so if any of them are done, do inform me!
+
+---
+
+1. ***[Teoalida : HDB flat types and their sizes](https://www.teoalida.com/singapore/hdbflattypes/)*** - I don't know the creator of this site and his site does look pretty scammy. I do understand he sell services on acquiring data. His explanations on different HDB flat and model types does look quite extensive.
+2. ***[Singapore Watch : HDB Flat Types & Models](http://singaporewatch.org/?p=582)*** - Another blog post about this topic. 
+""")
